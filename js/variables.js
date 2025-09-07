0bf5eea256e083153544667dba0cbd69f271e3c9
hedge <trenthancock123@gmail.com>
2025-09-06 20:25:56 -0400

    Update to 1.8.5

--- js/variables.js
+++ js/variables.js
@@ -2,19 +2,19 @@
 const MCYD2USBInvertOffFiles = {
     'bootloader': 'resources/STATIC/M/CYD/esp32_marauder.ino.bootloader.bin',
     'partitions': 'resources/STATIC/M/CYD/esp32_marauder.ino.partitions.bin',
-    'firmware': 'resources/CURRENT/esp32_marauder_v1_7_0_20250416_cyd2usb_gps_InvertOff.bin',
+    'firmware': 'resources/CURRENT/esp32_marauder_v1_8_5_20250416_cyd2usb_gps_InvertOff.bin',
 };
 
 // Latest Marauder for CYD2USB - Invert On
 const MCYD2USBInvertOnFiles = {
     'bootloader': 'resources/STATIC/M/CYD/esp32_marauder.ino.bootloader.bin',
     'partitions': 'resources/STATIC/M/CYD/esp32_marauder.ino.partitions.bin',
-    'firmware': 'resources/CURRENT/esp32_marauder_v1_7_0_20250416_cyd2usb_gps_InvertOn.bin',
+    'firmware': 'resources/CURRENT/esp32_marauder_v1_8_5_20250416_cyd2usb_gps_InvertOn.bin',
 };
 
 // Latest Marauder for CYD2USB V2.1 - Invert Off
 const MCYD2USBV21InvertOffFiles = {
     'bootloader': 'resources/STATIC/M/CYD/esp32_marauder.ino.bootloader.bin',
     'partitions': 'resources/STATIC/M/CYD/esp32_marauder.ino.partitions.bin',
-    'firmware': 'resources/CURRENT/esp32_marauder_v1_7_0_20250416_cyd2_1usb_gps_InvertOff.bin',
+    'firmware': 'resources/CURRENT/esp32_marauder_v1_8_5_20250416_cyd2_1usb_gps_InvertOff.bin',
 };
